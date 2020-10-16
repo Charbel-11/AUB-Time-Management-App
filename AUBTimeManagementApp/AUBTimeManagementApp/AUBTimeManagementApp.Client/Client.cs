2aa60282257bdc6c8011ca8b2a9b9e1f23fe93d8
Charbel Chucri <charbel11chucri@gmail.com>
2020-10-16 16:08:32 +0300

    Added some client functions

--- AUBTimeManagementApp/AUBTimeManagementApp/AUBTimeManagementApp.Client/Client.cs
+++ AUBTimeManagementApp/AUBTimeManagementApp/AUBTimeManagementApp.Client/Client.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AUBTimeManagementApp.AUBTimeManagementApp.DataContracts;
+using AUBTimeManagementApp.AUBTimeManagementApp.Service;
 
 namespace AUBTimeManagementApp.AUBTimeManagementApp.Client
 {
     class Client
     {
+        public string username;
+        private Schedule schedule;
+        private Team[] teams;
+
+        void CreateAccount(string username, string password, string email)
+        {
+            //TODO
+            this.username = username;
+        }
+
+        void LogIn(string username, string password)
+        {
+            //TODO
+            this.username = username;
+        }
+
+        void LogOut()
+        {
+
+        }
+
+        void ChangePassword(string oldPassword, string oldPasswordCheck, string newPassword)
+        {
+
+        }
     }
 }
---
+++ AUBTimeManagementApp/AUBTimeManagementApp/AUBTimeManagementApp.DataContracts/Schedule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AUBTimeManagementApp.AUBTimeManagementApp.DataContracts
+{
+    class Schedule
+    {
+    }
+}
