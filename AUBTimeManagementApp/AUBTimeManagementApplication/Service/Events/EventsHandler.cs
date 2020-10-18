99e60e3475cb6607e99c7f6be071b47b3d775e40
0-jij-0 <jihadghanna@gmail.com>
2020-10-18 23:51:37 +0300

    Fixed Braces :)

--- AUBTimeManagementApp/AUBTimeManagementApplication/Service/Events/EventsHandler.cs
+++ AUBTimeManagementApp/AUBTimeManagementApplication/Service/Events/EventsHandler.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AUBTimeManagementApp.DataContracts;
 
 namespace AUBTimeManagementApp.Service.Events
 {
     class EventsHandler
     {
+        public Event createEvent()
+        {
+
+        }
     }
 }
