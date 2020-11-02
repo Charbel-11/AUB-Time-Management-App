d88d67c98a7d2eb33b17b25d6aaf24cc28471a53
0-jij-0 <jihadghanna@gmail.com>
2020-11-03 01:08:19 +0200

    Created the Database and the different Tables needed along with the needed relations

---
+++ AUBTimeManagementApp/AUBTimeManagementApp/Service/Storage/dbo.Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[UserID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Username] NVARCHAR(50) NOT NULL, 
+    [First Name] NVARCHAR(50) NOT NULL, 
+    [Last Name] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [DateOfBirth] DATETIME NOT NULL
+)
