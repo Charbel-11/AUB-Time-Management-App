10c5e0b465ca24a91cd3c5633b723cbcda895412
Nourhane <49608714+Athena17@users.noreply.github.com>
2020-11-12 22:31:16 +0200

    Added the connection string

--- AUBTimeManagementApp/Server/Service/Storage/ConnectionUtil.cs
+++ AUBTimeManagementApp/Server/Service/Storage/ConnectionUtil.cs
@@ -7,5 +7,7 @@ using System.Threading.Tasks;
 namespace Server.Service.Storage {
     static class ConnectionUtil {
         public static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Lenovo\\OneDrive\\Desktop\\AUB-Time-Management-App\\AUBTimeManagementApp\\Server\\Service\\Storage\\AccountsDataBase.mdf;Integrated Security=True";
+        public static string connectionStringUniversalEventsDB = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Lenovo\\OneDrive\\Desktop\\AUB-Time-Management-App\\AUBTimeManagementApp\\Server\\Service\\Storage\\EventsUniversalDataBase.mdf;Integrated Security=True";
+
     }
 }
