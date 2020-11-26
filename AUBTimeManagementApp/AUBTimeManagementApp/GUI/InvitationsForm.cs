bc663daba0c6e34cf9ae01ee9821741c01a664f2
Nourhane <49608714+Athena17@users.noreply.github.com>
2020-11-26 17:28:59 +0200

    Working on the GUI of invitations

--- AUBTimeManagementApp/AUBTimeManagementApp/GUI/InvitationsForm.cs
+++ AUBTimeManagementApp/AUBTimeManagementApp/GUI/InvitationsForm.cs
@@ -32,23 +32,65 @@ namespace AUBTimeManagementApp.GUI
         }
         public void AddInvitationEntry(Invitation invitation)
         {
-            // Adding an accept button
-            Button newAccept = new Button();
-            newAccept.Margin = new Padding(0);
-            newAccept.Text = "Accept";
-            newAccept.Size = new Size(485, 46);
-            newAccept.Click += AcceptButton_Click;
-            flowLayoutPanel1.Controls.Add(newAccept);
-            AcceptButtonToInvitation[newAccept] = invitation;
 
-            // Adding a decline button
-            Button newDecline = new Button();
-            newDecline.Margin = new Padding(0);
-            newDecline.Text = "Decline";
-            newDecline.Size = new Size(485, 46);
-            newDecline.Click += DeclineButton_Click;
-            flowLayoutPanel1.Controls.Add(newDecline);
-            DeclineButtonToInvitation[newDecline] = invitation;
+            GroupBox groupBox1 = new GroupBox();
+            Button AcceptButton = new Button();
+            Button DeclineButton = new Button();
+            Label invitationLabel = new Label();
+
+            // 
+            // AcceptButton
+            // 
+            AcceptButton.BackColor = Color.LimeGreen;
+            AcceptButton.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            AcceptButton.ForeColor = Color.White;
+            AcceptButton.Location = new Point(155, 4);
+            AcceptButton.Name = "AcceptButton";
+            AcceptButton.Size = new Size(87, 27);
+            AcceptButton.TabIndex = 4;
+            AcceptButton.Text = "Accept";
+            AcceptButton.UseVisualStyleBackColor = false;
+            AcceptButton.Click += AcceptButton_Click;
+            // 
+            // DeclineButton
+            // 
+            DeclineButton.BackColor = Color.LightCoral;
+            DeclineButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            DeclineButton.ForeColor = System.Drawing.Color.White;
+            DeclineButton.Location = new System.Drawing.Point(260, 4);
+            DeclineButton.Name = "DeclineButton";
+            DeclineButton.Size = new Size(87, 27);
+            DeclineButton.TabIndex = 3;
+            DeclineButton.Text = "Decline";
+            DeclineButton.UseVisualStyleBackColor = false;
+            DeclineButton.Click += DeclineButton_Click;
+            // 
+            // invitationLabel
+            // 
+            invitationLabel.AutoSize = true;
+            invitationLabel.BackColor = SystemColors.ControlLight;
+            invitationLabel.ForeColor = Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            invitationLabel.Location = new Point(20, 4);
+            invitationLabel.Name = "invitationLabel";
+            invitationLabel.Size = new Size(36, 13);
+            invitationLabel.TabIndex = 2;
+            invitationLabel.Text = "Invitation";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(invitationLabel);
+            groupBox1.Controls.Add(AcceptButton);
+            groupBox1.Controls.Add(DeclineButton);
+            groupBox1.Location = new Point(0, 0);
+            groupBox1.Margin = new Padding(0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(361, 36);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+
+            flowLayoutPanel1.Controls.Add(groupBox1);
+            AcceptButtonToInvitation[AcceptButton] = invitation;
+            DeclineButtonToInvitation[DeclineButton] = invitation;
         }
 
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
