2a280febe084e39b7bfdd07799a0ed153b5600d2
Nourhane <49608714+Athena17@users.noreply.github.com>
2020-12-06 13:08:40 +0200

    Adding comments

--- AUBTimeManagementApp/Server/Service/Handlers/IAccountHandler.cs
+++ AUBTimeManagementApp/Server/Service/Handlers/IAccountHandler.cs
@@ -8,9 +8,16 @@ namespace Server.Service.Handlers
 {
     public interface IAccountsHandler
     {
+        // This function checks whether the registration info were valid to register the user
         int ConfirmRegistration(string username, string firstName, string lastName, string email, string password, string confirmPassword, DateTime dateOfBirth);
+        
+        // This function checks if the username and password are correct
         bool ConfirmLogIn(string username, string password);
+
+        // This function updates the password
         int ChangePassword(string username, string oldPassword, string newPassword, string confirmPassowrd);
+        
+        // This function checks whether there exists a user with a given username
         bool IsRegistered(string username);
     }
 }
